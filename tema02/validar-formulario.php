8110daef56f0f2205ea27a18ef79a5a1aa0cd886
robertorodriguezNet <robertorodriguez@robertorodriguez.net>
2023-10-27 13:10:09 +0200

    Listo para la tarea 02

--- tema02/validar-formulario.php
+++ tema02/validar-formulario.php
@@ -3,6 +3,15 @@ $title = "Validar formulario. DWES 02";
 
 $level = (sizeof(explode("/", $_SERVER["PHP_SELF"])) >= 4) ? "../" : "";
 include($level . "includes/head.php");
+
+function resultado()
+{
+    $resutl = 'Hay errores en el formulario';
+
+    // Recibimos nombre, apellidos, email y edad
+
+    return $resutl;
+}
 ?>
 
 <h2>DWES 02</h2>
@@ -28,15 +37,42 @@ include($level . "includes/head.php");
         <input type="text" name="apellidos" id="apellidos" maxlength="50" required>
         <br>
         <label for="email" id="email">Email: </label>
-        <input type="text" name="email" id="email" maxlength="100" required>
+        <input type="email" name="email" id="email" maxlength="100" required>
         <br>
         <label for="edad" id="edad">Edad: </label>
         <input type="number" name="edad" id="edad" max="99" min="16" required>
+        <fieldset>
+        <legend>Módulos</legend>
+        <label for="modulo" id="daw" value="daw">
+            <input type="checkbox" name="modulo" id="daw" value="daw">
+            DAW
+        </label>
+        <br>
+        <label for="modulo" id="dwec" value="dewc">
+            <input type="checkbox" name="modulo" id="daw" value="dwec">
+            DWEC
+        </label>
+        <br>
+        <label for="modulo" id="daw" value="dwes">
+            <input type="checkbox" name="modulo" id="dwes" value="dwes">
+            DEWS
+        </label>
+        <br>
+        <label for="modulo" id="daw" value="diw">
+            <input type="checkbox" name="modulo" id="diw" value="diw">
+            DIW
+        </label>
+
+    </fieldset>
         <br>
         <input type="submit" value="Enviar">
     </form>
 
+
 </fieldset>
+
+<?php if(isset($_POST['nombre'])) echo resultado() ?>
+
 
 
 <?php
